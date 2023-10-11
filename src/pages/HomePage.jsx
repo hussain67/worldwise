021433b3ce0fbe472c9ab6956551ebb68815b862
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2023-10-11 02:00:03 +0100

    Css file name corrected

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./Homepage.module.css";
+import styles from "./HomePage.module.css";
 import PageNav from "../components/PageNav";
 import { useAuth0 } from "@auth0/auth0-react";
 
